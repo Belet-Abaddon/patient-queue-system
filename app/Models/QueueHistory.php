ea9b103c5a4e3f39f3d1d59691080e4ad89c03b6
Belet-Abaddon <sawmyatmonhnin1420@gmail.com>
2025-12-09 20:54:13 +0630

    First commit

---
+++ app/Models/QueueHistory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QueueHistory extends Model
+{
+    /** @use HasFactory<\Database\Factories\QueueHistoryFactory> */
+    use HasFactory;
+}
---
+++ database/migrations/2025_12_09_130428_create_queue_histories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('queue_histories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_id')->constrained('patients')->onDelete('cascade');
+            $table->foreignId('doctor_id')->constrained('doctors')->onDelete('cascade'); 
+            $table->timestamp('call_time'); 
+            $table->string('status')->default('served');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('queue_histories');
+    }
+};
